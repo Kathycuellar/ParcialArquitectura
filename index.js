e7013e5b9493a1b1bff369ec1013aa0b8e10e8d5
Kathycuellar <35872993+Kathycuellar@users.noreply.github.com>
2018-02-24 12:19:59 -0600

    Update index.js

--- index.js
+++ index.js
@@ -3,11 +3,11 @@ const TelegramBot = require('node-telegram-bot-api');
  // API Token Telegram
  const token = '498041939:AAHEccvg_hHlR8kvU6pzNijuP2KW30NcfPQ';
 
- // Creamos un bot que usa 'polling'para obtener actualizaciones
+ 
 const bot = new TelegramBot(token, {polling: true});
  const request = require('request');
 
-// Cuando mandes el mensaje "Hola" reconoce tú nombre y genera un input: Hola Daniel
+
  bot.on('message', (msg) => {
  var Hola = "/hola";
  if (msg.text.toString().toLowerCase().indexOf(Hola) === 0) {
