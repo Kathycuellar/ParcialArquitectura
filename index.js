b4a429aa0516558e12c9d409e9271974b316396e
Indfernox <baltacarbonero@gmail.com>
2018-02-24 13:01:51 -0600

    1) todas las preguntas
    Responder muy pronto lanzamiento bot
    Estudiante: Marlon Carbonero

--- index.js
+++ index.js
@@ -3,7 +3,7 @@ const TelegramBot = require('node-telegram-bot-api');
  // API Token Telegram
  const token = '498041939:AAHEccvg_hHlR8kvU6pzNijuP2KW30NcfPQ';
 
- 
+
 const bot = new TelegramBot(token, {polling: true});
  const request = require('request');
 
@@ -12,5 +12,9 @@ const bot = new TelegramBot(token, {polling: true});
  var Hola = "/hola";
  if (msg.text.toString().toLowerCase().indexOf(Hola) === 0) {
      bot.sendMessage(msg.chat.id, "Hola  " + msg.from.first_name);
+ }
+ else if (msg.text.toString().toLowerCase().indexOf("?") > -1) {
+      bot.sendMessage(msg.chat.id, "Muy pronto lanzamiento bot.");
+  }
  }
  });
